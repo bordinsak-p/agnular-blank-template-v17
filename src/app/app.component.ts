59af9b0c48631661c98089f59554c4d2b0206c9b
MeanBordin <bodinsak2544@gmail.com>
2025-05-05 02:25:38 +0700

    update sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,31 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BreadcrumbModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BreadcrumbModule,
+    AvatarModule,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   items: MenuItem[] | undefined;
-
   home: MenuItem | undefined;
+
+  firstname = 'บดินทร์ศักดิ์';
+  lastname = 'ประสพบุญ';
+  position = 'ผู้พัฒนาระบบ';
+  isSidebarVisible = true;
 
   ngOnInit() {
     this.items = [
@@ -26,5 +38,11 @@ export class AppComponent {
     ];
 
     this.home = { icon: 'pi pi-home', routerLink: '/' };
+  }
+
+  toggleSidebar() {
+    console.log('Toggle sidebar clicked', this.isSidebarVisible);
+    
+    this.isSidebarVisible = !this.isSidebarVisible;
   }
 }
